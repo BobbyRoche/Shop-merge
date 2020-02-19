24b8635e2460e0967bb89bedaacbc51eea828679
rr696 <rr696@drexel.edu>
2020-02-19 05:09:04 -0500

    added stuff

---
+++ Assets/logic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class logic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public score_board s;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "Item")
+        {
+            s.score += 1;
+        }
+    }
+}
---
+++ Assets/score_board.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score_board : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text t;
+    public int score;
+    // Update is called once per frame
+    void Update()
+    {
+        t.text = "Score: "+score.ToString();
+    }
+}
